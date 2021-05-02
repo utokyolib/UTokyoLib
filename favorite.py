314d09f701b031ebe631dfc42e8b2bfd5ac6254c
HelloRusk <hellorusk1998@gmail.com>
2021-05-02 10:45:25 +0900

    refactor: favorite.py

--- favorite.py
+++ favorite.py
@@ -14,88 +14,39 @@ twitter = tweepy.API(auth)
 
 logging.basicConfig(level=logging.INFO)
 
-# 「駒場 図書館」で検索
-komaba_tweets = twitter.search("駒場 図書館", result_type="recent")
+queries = [
+    "東大 図書館",
+    "総合図書館",
+    "柏図書館",
+    "本郷 図書館",
+    "駒場 図書館"
+]
 
-for tweet in komaba_tweets:
+for query in queries:
 
-    # 既にファボしていたら skip する
-    if tweet.favorited:
-        continue
+    tweets = twitter.search(query, result_type="recent")
 
-    # URL を含むツイートは対象外にする
-    if tweet.entities["urls"]:
-        continue
+    for tweet in tweets:
 
-    # リプライは対象外にする
-    if tweet.in_reply_to_status_id or tweet.in_reply_to_user_id:
-        continue
+        # 既にファボしていたら skip する
+        if tweet.favorited:
+            continue
 
-    # 自分自身のツイートは対象外にする
-    if tweet.user.screen_name == "UTokyoLib_bot":
-        continue
+        # URL を含むツイートは対象外にする
+        if tweet.entities["urls"]:
+            continue
 
-    try:
-        twitter.create_favorite(tweet.id)
-        logging.info(f"{tweet.user.screen_name} さんによる以下のツイートをファボしました: ")
-        logging.info(tweet.text)
-    except tweepy.error.TweepError as e:
-        logging.info(e)
+        # リプライは対象外にする
+        if tweet.in_reply_to_status_id or tweet.in_reply_to_user_id:
+            continue
 
+        # 自分自身のツイートは対象外にする
+        if tweet.user.screen_name == "UTokyoLib_bot":
+            continue
 
-# 「本郷 図書館」で検索
-hongo_tweets = twitter.search("本郷 図書館", result_type="recent")
-
-for tweet in hongo_tweets:
-
-    # 既にファボしていたら skip する
-    if tweet.favorited:
-        continue
-
-    # URL を含むツイートは対象外にする
-    if tweet.entities["urls"]:
-        continue
-
-    # リプライは対象外にする
-    if tweet.in_reply_to_status_id or tweet.in_reply_to_user_id:
-        continue
-
-    # 自分自身のツイートは対象外にする
-    if tweet.user.screen_name == "UTokyoLib_bot":
-        continue
-
-    try:
-        twitter.create_favorite(tweet.id)
-        logging.info(f"{tweet.user.screen_name} さんによる以下のツイートをファボしました: ")
-        logging.info(tweet.text)
-    except tweepy.error.TweepError as e:
-        logging.info(e)
-
-
-# 「総合図書館」で検索
-sogo_tweets = twitter.search("総合図書館", result_type="recent")
-
-for tweet in sogo_tweets:
-
-    # 既にファボしていたら skip する
-    if tweet.favorited:
-        continue
-
-    # URL を含むツイートは対象外にする
-    if tweet.entities["urls"]:
-        continue
-
-    # リプライは対象外にする
-    if tweet.in_reply_to_status_id or tweet.in_reply_to_user_id:
-        continue
-
-    # 自分自身のツイートは対象外にする
-    if tweet.user.screen_name == "UTokyoLib_bot":
-        continue
-
-    try:
-        twitter.create_favorite(tweet.id)
-        logging.info(f"{tweet.user.screen_name} さんによる以下のツイートをファボしました: ")
-        logging.info(tweet.text)
-    except tweepy.error.TweepError as e:
-        logging.info(e)
+        try:
+            twitter.create_favorite(tweet.id)
+            logging.info(f"{tweet.user.screen_name} さんによる以下のツイートをファボしました: ")
+            logging.info(tweet.text)
+        except tweepy.error.TweepError as e:
+            logging.info(e)
